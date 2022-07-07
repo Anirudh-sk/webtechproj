122814520e6d2180caa66d6cf6b2e4e9237efb82
ani <amazinganirudhhere@gmail.com>
2022-07-08 01:16:02 +0530

    port

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express()
 app.use(express.urlencoded({extended: true}))
 
 
-const PORT= 3000
+const PORT= process.env.PORT || 3000
 const upload= multer({dest: "uploads"})
 mongoose.connect(process.env.Database_URL)
 
